5a88fbc31bc875d54a2d91ae7724ffa31b6d5815
ashishkumarsinghh <ashishkumarsinghh@outlook.com>
2018-05-25 22:06:15 +0530

    Added message styles, fixed multiple broadcast issue.

--- public/client.js
+++ public/client.js
@@ -1,9 +1,18 @@
 var socket = io();
 
 function send(){
-  socket.emit("message", document.getElementById("msg").value);
-  document.getElementById("msg").value ="";
-  $('#chatarea').animate({scrollTop: $('#chatarea').prop("scrollHeight")}, 500);
+  let msg_val = document.getElementById("msg").value;
+  if(msg_val.startsWith("join")){
+    let username = msg_val.substr(5);
+    socket.emit("join",username);
+    document.getElementById("msg").value ="";
+  }
+  else{
+    socket.emit("message", msg_val);
+    document.getElementById("msg").value ="";
+    $('#chatarea').animate({scrollTop: $('#chatarea').prop("scrollHeight")}, 500);
+  }
+
 }
 
 function enterSend(event){
@@ -12,8 +21,44 @@ function enterSend(event){
   }
 }
 
+socket.on("join", function(username){
+  let notif = document.createElement("DIV");
+  notif.classList.add("ui");
+  notif.classList.add("success");
+  notif.classList.add("message");
+  let notif_header = document.createElement("DIV");
+  notif_header.classList.add("header");
+  notif_header.appendChild(document.createTextNode(username + " has just joined."));
+  notif.appendChild(notif_header);
+  document.getElementById("messages").appendChild(notif);
+});
+
+socket.on("warning", function(warning_msg){
+  let wtext = document.createElement("DIV");
+  wtext.classList.add("ui");
+  wtext.classList.add("negative");
+  wtext.classList.add("message");
+  let wtext_header = document.createElement("DIV");
+  wtext_header.classList.add("header");
+  wtext_header.appendChild(document.createTextNode(warning_msg));
+  wtext.appendChild(wtext_header);
+  document.getElementById("messages").appendChild(wtext);
+});
 socket.on("message", function(msg){
+
+  let dtext = document.createElement("DIV");
+  dtext.classList.add("ui");
+  dtext.classList.add("small");
+  dtext.classList.add("message");
+
+  let dtext_header = document.createElement("DIV");
+  dtext_header.classList.add("header");
+  dtext_header.appendChild(document.createTextNode(msg.username));
+
   let ptext = document.createElement("P");
   ptext.appendChild(document.createTextNode(msg.msg));
-  document.getElementById("messages").appendChild(ptext);
+
+  dtext.appendChild(dtext_header);
+  dtext.appendChild(ptext);
+  document.getElementById("messages").appendChild(dtext);
 });
--- server.js
+++ server.js
@@ -8,10 +8,21 @@ app.use(express.static('public'))
 app.get('/',(req, res) => res.sendFile(path.resolve('./index.html')))
 
 io.on('connection', function(socket){
-  socket.on("message", function(msgs){
-    const msg = {"msg":msgs,"id":"stranger"};
-    io.emit("message",msg);
+  socket.on("join", function(username){
+    socket.username = username;
+    io.emit("join", socket.username);
   });
+
+  socket.on("message", function(msgs){
+    if(socket.username !== undefined && socket.username !== null && socket.username.length>0){
+      const msg = {"msg":msgs,"username":socket.username};
+      io.emit("message",msg);
+    }
+    else{
+      io.to(socket.id).emit("warning","Kindly join first by typing <join> <SPACE> <username>.");
+    }
+  });
+
 });
 
 http.listen(3000, function(){
